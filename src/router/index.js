1a399d08ae686f7c5d828c6abc55a8da167db67b
Spec Ussa <115887844+Specussa@users.noreply.github.com>
2024-02-24 04:37:13 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ import OfferView from '../views/OfferView.vue'
 import Tr from "@/i18n/translation"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/:locale?",
